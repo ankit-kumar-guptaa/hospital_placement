3e1a882cb5710a56ee05b550c685c1ef8a37daea
ANKIT KUMAR GUPTA <65163759+ankit-kumar-guptaa@users.noreply.github.com>
2025-02-17 10:20:24 +0530

    Set default fetch mode to associative array in database connection

--- include/db.php
+++ include/db.php
@@ -16,6 +16,8 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    // echo "Connected successfully";
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
